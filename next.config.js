a8bcfd9bcb42e8df703de9b4eb78f6872a7eb222
Ghaithbs <ghaithbs300@gmail.com>
2026-08-12 23:04:41 +0100

    Fix: Clean next.config.js syntax

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-@"
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // If you get 404 errors, add this:
-  // basePath: '/tulum',
-  // assetPrefix: '/tulum',
 }
 
 module.exports = nextConfig
-"@ | Out-File -FilePath next.config.js -Encoding utf8
